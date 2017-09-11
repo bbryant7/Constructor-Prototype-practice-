f2a2d8d2631dd6559287816210b46b1b488ceaf5
Bailey Bryant <bailey.bryant10@gmail.com>
2017-09-11 14:43:50 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+//###################################################################
+//        __
+//   ____/ /___  ____ ______
+//  / __  / __ \/ __ `/ ___/
+// / /_/ / /_/ / /_/ (__  )
+// \__,_/\____/\__, /____/
+//            /____/
+
+// Dog Constructor & Prototype
+function Dog (status, color, hungry, owner) {
+ this.status = status;
+ this.color = color;
+ this.hungry = hungry;
+ this.owner = owner;
+}
+
+let sadie = new Dog("normal", "black", false, "Mason");
+let moonshine = new Dog("", "", true);
+let atticus = new Dog ("", "", false);
+// Instances of Dog
+// Needed: sadie, moonshine, atticus
+
+
+
+//     __
+//    / /_  __  ______ ___  ____ _____  _____
+//   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
+//  / / / / /_/ / / / / / / /_/ / / / (__  )
+// /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
+
+// Human Constructor & Prototype
+function Human (pet, feed, cool) {
+this.cool = cool;
+this.pet = function (dog){
+  return  dog.status = "happy"
+  }
+
+
+this.feed = function (dog){
+    return dog.hungry = false
+
+}
+}
+
+// Instances of Human
+// Needed: mason, julia
+
+let mason = new Human(true, false, false);
+let julia = new Human(false, true, true);
